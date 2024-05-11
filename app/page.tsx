5dc1ceef8bbb8b0d3d82fa5a7f16c6034a3dfc06
jtamu <gtamu.dip@gmail.com>
2024-05-12 00:09:27 +0900

    feat: ミドルウェアでログインを強制

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       {!user && <a href="/api/auth/login" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ログイン</a>}
       {user && <a href="/api/auth/logout" className="inline-block bg-white hover:bg-gray-200 text-gray-800 font-bold py-2 px-4 border border-gray-300 rounded shadow">ログアウト</a>}
       <ProfileClient />
-      {user && <Link href="/profile" className="text-blue-500 hover:text-blue-700 hover:underline">プロフィール</Link>}
+      {user && <Link href="/user/profile" className="text-blue-500 hover:text-blue-700 hover:underline">プロフィール</Link>}
       Hello World
     </main>
   );
--- app/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
-
-export default withPageAuthRequired(async function Profile() {
-  const session = await getSession();
-  const user = session?.user
-  return <div>Hello {user?.name}</div>;
-}, { returnTo: '/profile' })
