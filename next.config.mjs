9177f59f321cf3e1f7e768fa7eac621875e1c67c
jtamu <gtamu.dip@gmail.com>
2024-06-28 00:23:33 +0900

    feat: githubの画像を許可

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["s.gravatar.com", "lh3.googleusercontent.com"]
+        domains: ["s.gravatar.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com"]
     }
 };
 
