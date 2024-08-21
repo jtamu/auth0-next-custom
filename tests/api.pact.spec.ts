c4ab6eb725e20eb7f13763879df65bc6c523a3d2
jtamu <gtamu.dip@gmail.com>
2024-08-22 01:05:02 +0900

    feat: GET /auth0/micropostsのpactテストを実装

--- tests/api.pact.spec.ts
+++ tests/api.pact.spec.ts
@@ -2,6 +2,8 @@ import MicropostService from "@/app/api/microposts/service";
 import { MatchersV3, PactV3 } from "@pact-foundation/pact";
 import path from "path";
 
+const {eachLike, string, regex, timestamp} = MatchersV3;
+
 const provider = new PactV3({
     dir: path.resolve(process.cwd(), 'pacts'),
     consumer: 'auth0-next-custom',
@@ -31,3 +33,54 @@ describe('GET /ping', () => {
         })
     })
 });
+
+describe('GET /auth0/microposts', () => {
+    it('micropostsを返すこと', async () => {
+        provider.addInteraction({
+            states: [{description: 'Authorizationヘッダが正しく設定されている場合'}],
+            uponReceiving: 'ユーザの全ての投稿をリクエストする',
+            withRequest: {
+                method: 'GET',
+                path: '/auth0/microposts',
+                headers: {'Authorization': regex('^Bearer\\s+[\\w-]+$', 'Bearer token123')},
+            },
+            willRespondWith: {
+                status: 200,
+                headers: {'Content-Type': 'application/json'},
+                body: eachLike({
+                    content: string('Hello, World.'),
+                    postedAt: timestamp('YYYY-MM-DD HH:mm:ss', '2024-08-21 23:01:01'),
+                })
+            }
+        });
+
+        await provider.executeTest(async (mockserver) => {
+            const service = MicropostService(mockserver.url);
+            const response = await service.getAll('hoge');
+            expect(JSON.stringify(response)).toStrictEqual(
+                JSON.stringify([{content: 'Hello, World.', postedAt: '2024-08-21 23:01:01'}])
+            );
+        })
+    });
+
+    it('401を返すこと', async () => {
+        provider.addInteraction({
+            // Authorizationヘッダがない場合は、MicropostServiceの設計上起こり得ない
+            states: [{description: 'Authorizationヘッダが不正な場合'}],
+            uponReceiving: 'ユーザの全ての投稿をリクエストする',
+            withRequest: {
+                method: 'GET',
+                path: '/auth0/microposts',
+                headers: {'Authorization': regex('^Bearer\\s+[\\w-]+$', 'Bearer token123')},
+            },
+            willRespondWith: {
+                status: 401
+            }
+        });
+
+        await provider.executeTest(async (mockserver) => {
+            const service = MicropostService(mockserver.url);
+            await expect(service.getAll('hoge')).rejects.toThrow(/401/)
+        })
+    })
+})
